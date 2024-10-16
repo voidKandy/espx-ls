0359dc08e36b454eff4a58c3a505e723aa5c93ed
voidKandy <voidkandy@gmail.com>
2024-10-16 09:26:58 -0700

    working agents section again

--- gui/src/ui/agents.rs
+++ gui/src/ui/agents.rs
@@ -2,7 +2,7 @@ use std::{collections::HashSet, str::FromStr};
 
 use eframe::egui;
 
-use egui::{TextBuffer, Ui};
+use egui::{SelectableLabel, TextBuffer, Ui};
 use egui_extras::{Column, TableBuilder};
 use espionox::prelude::*;
 use lsp_types::Uri;
@@ -64,16 +64,18 @@ impl EditingAgent {
 
 #[derive(Debug)]
 pub struct AgentsSectionState {
-    // current_agent_name: String,
-    should_get_editing: bool,
+    all_names: HashSet<String>,
+    should_switch_to_agent: Option<String>,
     editing_agent: Option<EditingAgent>,
 }
+
+const GLOBAL_AGENT_NAME: &str = "Global";
 
 impl Default for AgentsSectionState {
     fn default() -> Self {
         Self {
-            // current_agent_name: "Global".to_string(),
-            should_get_editing: true,
+            all_names: HashSet::new(),
+            should_switch_to_agent: Some(GLOBAL_AGENT_NAME.to_string()),
             editing_agent: None,
         }
     }
@@ -81,11 +83,12 @@ impl Default for AgentsSectionState {
 
 fn get_all_names(agents: &Agents) -> HashSet<String> {
     let mut all_names = HashSet::new();
-    all_names.insert("Global".to_string());
+    all_names.insert(GLOBAL_AGENT_NAME.to_string());
 
     for ch in all_custom_names(agents).iter() {
         all_names.insert(ch.to_string());
     }
+
     for uri in all_doc_names(agents).iter() {
         all_names.insert(uri.to_string());
     }
@@ -111,31 +114,34 @@ impl AgentsSectionState {
         let get_global = |a: &mut Agents| -> EditingAgent {
             EditingAgent::from_agent_and_id(a.global_agent_ref(), AgentID::Global)
         };
-        if self.editing_agent.is_none() | self.should_get_editing {
-            if let Some(current_agent_name) = self.current_agent_name() {
-                warn!("current agent: {current_agent_name}");
-                self.editing_agent = match current_agent_name.as_str() {
-                    "Global" => Some(get_global(agents)),
-                    _ if all_custom_names(agents).contains(&current_agent_name) => {
-                        let char = current_agent_name.chars().next().unwrap();
-                        agents
-                            .custom_agent_ref(char)
-                            .ok()
-                            .and_then(|ag| Some(EditingAgent::from_agent_and_id(ag, char)))
-                    }
-                    _ if all_doc_names(agents).contains(&current_agent_name) => {
-                        let uri = Uri::from_str(current_agent_name.as_str())
-                            .expect("could not make uri from string");
-                        agents
-                            .doc_agent_ref(&uri)
-                            .ok()
-                            .and_then(|ag| Some(EditingAgent::from_agent_and_id(ag, uri)))
-                    }
-                    _ => None,
-                };
-            }
 
-            self.should_get_editing = false;
+        let all_names = get_all_names(agents);
+        if all_names != self.all_names {
+            warn!("updating all names: {all_names:#?}");
+            self.all_names = all_names
+        }
+
+        if let Some(switch_to_agent) = self.should_switch_to_agent.take() {
+            self.editing_agent = match switch_to_agent.as_str() {
+                GLOBAL_AGENT_NAME => Some(get_global(agents)),
+                _ if all_custom_names(agents).contains(&switch_to_agent) => {
+                    let char = switch_to_agent.chars().next().unwrap();
+                    agents
+                        .custom_agent_ref(char)
+                        .ok()
+                        .and_then(|ag| Some(EditingAgent::from_agent_and_id(ag, char)))
+                }
+                _ if all_doc_names(agents).contains(&switch_to_agent) => {
+                    let uri = Uri::from_str(switch_to_agent.as_str())
+                        .expect("could not make uri from string");
+                    agents
+                        .doc_agent_ref(&uri)
+                        .ok()
+                        .and_then(|ag| Some(EditingAgent::from_agent_and_id(ag, uri)))
+                }
+                _ => None,
+            };
+
             if self.editing_agent.is_none() {
                 self.editing_agent = Some(get_global(agents));
             }
@@ -144,7 +150,7 @@ impl AgentsSectionState {
 
     fn current_agent_name(&self) -> Option<String> {
         match &self.editing_agent.as_ref()?.id {
-            AgentID::Global => Some("Global".to_string()),
+            AgentID::Global => Some(GLOBAL_AGENT_NAME.to_string()),
             AgentID::Char(s) | AgentID::Uri(s) => Some(s.to_string()),
         }
     }
@@ -153,25 +159,28 @@ impl AgentsSectionState {
 pub fn render_agents_section(ui: &mut Ui, app: &mut super::App) {
     tracing::warn!("render agent section");
     let mut guard = app.state.get_write().unwrap();
-    // app.agents_section.agents = guard.agents.as_mut();
     match guard.agents.as_mut() {
         Some(agents) => {
             app.agents_section.update(agents);
-            let all_names = get_all_names(agents);
-            // app.agents_section.current_agent_name = all_names[0].to_string();
-            if let Some(ref mut current_agent_name) =
-                &mut app.agents_section.current_agent_name().as_mut()
-            {
-                for name in all_names {
-                    if ui
-                        .add(egui::SelectableLabel::new(
-                            **current_agent_name == name,
-                            current_agent_name.to_string().as_str(),
-                        ))
-                        .clicked()
-                    {
-                        **current_agent_name = name;
-                        app.agents_section.should_get_editing = true;
+
+            let selectable_labels = |current_name: &str| -> Vec<(SelectableLabel, &String)> {
+                app.agents_section
+                    .all_names
+                    .iter()
+                    .map(|n| {
+                        (
+                            egui::SelectableLabel::new(current_name == n, n.to_string().as_str()),
+                            n,
+                        )
+                    })
+                    .collect()
+            };
+
+            if let Some(current_agent_name) = app.agents_section.current_agent_name() {
+                for (label, name) in selectable_labels(&current_agent_name) {
+                    if ui.add(label).clicked() {
+                        warn!("clicked label. Changing current name to {name}");
+                        app.agents_section.should_switch_to_agent = Some(name.to_string());
                     }
                 }
             }
